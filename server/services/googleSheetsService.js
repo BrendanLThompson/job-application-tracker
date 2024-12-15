0e19b45375b12f689a965197e6d6bed75ced24b8
Brendan Thompson <49353402+BrendanLThompson@users.noreply.github.com>
2024-12-15 01:59:01 -0800

    Update googleSheetsService.js

--- server/services/googleSheetsService.js
+++ server/services/googleSheetsService.js
@@ -8,7 +8,7 @@ const credentials = JSON.parse(
 const range = "Sheet1!A1:E";
 
 const SPREADSHEET_ID =
-  "YOUR SPREADSHEET ID HERE";
+  "1rsZW3CYoSQPTDvL4J-Y2ZeEYxFJli5fpNMvA_ESuLlY";
 
 const auth = new google.auth.GoogleAuth({
   credentials,
